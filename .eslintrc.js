fcd9d55c412ec90a2ee7b51317b434f9a57c1875
澤田羽衣 <38057354+wc-sawada@users.noreply.github.com>
2021-04-12 00:55:08 +0900

    TODOリスト

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'vue/comment-directive': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/no-v-html': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 5,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
+  }
+}
